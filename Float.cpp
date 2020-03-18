01047bc636cc4d9b3f5d7a05019bc376f63d1d71
Uğur Emre Arıkan <uuguremre@gmail.com>
2020-03-19 01:42:30 +0300

    Add files via upload

---
+++ Float.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main ()
+{
+  //float Değişkenler
+  //%f
+
+  /*float sayi;
+  sayi=5;
+  printf("%f",sayi); */
+
+ /* float deg1,deg2,toplam;
+  deg1=5.85;
+  deg2=4.37;
+  toplam=deg1+deg2;
+  printf("Toplam:%f",toplam);*/
+
+  /*float sinav1,sinav2,sinav3,ort;
+  sinav1=75;
+  sinav2=80;
+  sinav3=81;
+  ort=(sinav1+sinav2+sinav3)/3;
+  printf("Ortalama :%f",ort); */
+
+
+
+
+
+  return 0;
+}
