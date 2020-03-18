8417e2a44b5d1a5666bd1a6393e55412c5b9549a
Uğur Emre Arıkan <uuguremre@gmail.com>
2020-03-19 01:43:27 +0300

    Add files via upload

---
+++ dongu1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+int main(void) {
+	//Faktöriyel hesaplayan bir for döngüsü
+	/*int num;
+	printf("sayiyi giriniz:");
+	scanf("%d", &num);
+	int fact = 1;
+	for (int i = 1; i <= num; i++)
+	{
+		fact = fact * i;
+	}
+	printf("\n %d! = %d",num,fact);
+	*/
+	/*
+	while (1)
+	{
+		printf("Sonsuz dongu");
+	}
+	
+	*/
+	getch();
+	return 0;
+}
